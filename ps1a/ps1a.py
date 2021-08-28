51ef0ab7470161dc5179579db2f2493a91656edb
Aaron Reid <areid88@gmail.com>
2021-08-27 20:03:58 -0700

    Update ps1a.py

--- ps1a/ps1a.py
+++ ps1a/ps1a.py
@@ -3,20 +3,21 @@ import math
 
 a = input("Cost of your dream home: ") #total_cost of dream home
 total_cost = float(a)
-portion_down_payment = total_cost * float(0.25)
+down_payment = total_cost * float(0.25)
 b = input("Please enter your annual salary: ") #annual_salary
 annual_salary = float(b)
 current_savings = annual_salary / 12
 r = current_savings * float(0.04)
-c = input("Please enter the amount of your salary to be saved: ") #protion_saved
-protion_saved = float(c)
+c = input("Please enter the amount of your salary to be saved, as a decimal: ") #protion_saved
+protion_saved = current_savings * float(c)
 monthly_salary = annual_salary/12
 end_of_month = r + protion_saved
 
 
 
-x = portion_down_payment / end_of_month
-y = str(x/12)
+x = str(down_payment / end_of_month) # how long it takes to save for the down payment.
+#y = str(total_cost - x)
+#y = str(x/12)
 
 
-print("It will take " + y + "years to save your down payment!")
+print("It will take " + x + " months to save your down payment!")
